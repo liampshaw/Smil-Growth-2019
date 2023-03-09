7b0aeb8942e7ebb641fcf6d00a4ad23b1b13d50c
Liam Shaw <liam.philip.shaw@gmail.com>
2023-03-09 13:45:24 +0000

    using csv in python to make latex

--- scripts/makeLatex.py
+++ scripts/makeLatex.py
@@ -1,15 +1,15 @@
-import re
-
+import csv
 
 
 with open("scripts/latex-header.tex", "r") as f:
 	for line in f.readlines():
 		print(line.strip("\n"))
 
+
 with open("data/datasets.csv", "r") as f:
-	for i, line in enumerate(f.readlines()):
+	for i, split_line in enumerate(csv.reader(f, quotechar='"', delimiter=',',
+                     quoting=csv.QUOTE_ALL, skipinitialspace=True)):
 		if i!=0:
-			split_line = re.split(",", line)
 			if split_line[1]!="yes":
 				print("\\begin{figure}[h]")
 				print("\includegraphics[width=8cm]{output/figs-ggplot/"+split_line[0]+".pdf}")
